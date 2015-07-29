7ee31215800f2f65297494869d464ae1f2ad96c3
samuelralak <samuelralak@hotmail.com>
2015-07-29 23:42:24 +0300

    list views

--- app/src/main/java/com/example/samuel/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/samuel/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -18,6 +23,34 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/40",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Asteroids - 75/65",
+                "Fri - Heavy Rain - 65/56",
+                "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
+                "Sun - Sunny - 80/68"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(
+                Arrays.asList(forecastArray)
+        );
+
+        // Now that we have some dummy forecast data, create an ArrayAdapter.
+        // The ArrayAdapter will take data from source (like our dummy forecast) and
+        // use it to populate the list view its attached to
+        mForecastAdapter = new ArrayAdapter<String>(
+                //the current context (this fragments parent activity)
+                getActivity(),
+                // ID of list item layout
+                R.layout.list_item_forecast,
+                // ID of the textview to populate
+                R.id.list_item_forecast_textview,
+                // Forecast data
+                weekForecast
+        );
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
